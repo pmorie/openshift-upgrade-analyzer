da3c32e8e857c297329ff2a591c46c1ae7e0e2e0
Fotios Lindiakos <fotios@redhat.com>
2013-09-20 09:30:29 -0400

    Minor usability fixes
    - Fixed finding correct app to upload to
    - Wrote helper script for executing commands

--- upload.rb
+++ upload.rb
@@ -1,6 +1,8 @@
 #!/bin/env ruby
 require 'tmpdir'
 require 'fileutils'
+require 'uri'
+require 'pty'
 
 label = ARGV[0]
 urls = ARGV[1..-1]
@@ -10,9 +12,24 @@ unless urls && urls.length > 1
   exit 1
 end
 
+def run_cmd(cmd, msg = nil)
+  puts msg if msg
+  begin
+    PTY.spawn( cmd ) do |stdin, stdout, pid|
+      begin
+        # Do stuff with the output here. Just printing to show it works
+        stdin.each { |line| print line }
+      rescue Errno::EIO
+      end
+    end
+  rescue PTY::ChildExited
+  end
+end
+
 puts "Using label '#{label}'"
 
-scp_host = `rhc app show -pp | grep SSH:`.match(/SSH:\s+([^\s]+)$/)[1]
+ssh_info = `rhc apps`.scan(/Git URL:\s+(.*)\n\s+Initial Git URL:.*openshift-upgrade-analyzer.git/).flatten.map{|x| URI.parse(x) }.first
+scp_host = "#{ssh_info.user}@#{ssh_info.host}"
 
 Dir.mktmpdir do |tmp|
   upload_dir = File.join(tmp, label)
@@ -21,15 +38,13 @@ Dir.mktmpdir do |tmp|
   Dir.chdir(upload_dir) do
     cmd = 'curl'
     urls.each {|url| cmd << " -O #{url}"}
-    puts "Downloading #{urls.length} files to #{tmp}..."
-    puts `#{cmd} 2>&1`
+    run_cmd cmd, "Downloading #{urls.length} files to #{tmp}..."
 
-    puts "Uploading #{urls.length} files to #{scp_host}..."
     cmd = "scp -r #{upload_dir} #{scp_host}:~/app-root/data/uploads/#{label}"
-    puts `#{cmd} 2>&1`
+    run_cmd cmd, "Uploading #{urls.length} files to #{scp_host}..."
 
-    puts "Marking upload ready..."
-    puts `ssh #{scp_host} "touch ~/app-root/data/uploads/#{label}/upload.ready" 2>&1`
+    cmd = "ssh #{scp_host} 'touch ~/app-root/data/uploads/#{label}/upload.ready' 2>&1"
+    run_cmd cmd, "Marking upload ready..."
   end
 end
 
